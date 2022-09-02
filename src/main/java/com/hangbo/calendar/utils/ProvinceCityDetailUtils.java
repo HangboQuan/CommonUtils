39666b52d6444d9a5b5e3e74a384ea0857435a9c
quanhangbo <2409711401@qq.com>
2022-09-02 20:48:07 +0800

    province and city json data(some data are missing)

--- src/main/java/com/hangbo/calendar/utils/ProvinceCityDetailUtils.java
+++ src/main/java/com/hangbo/calendar/utils/ProvinceCityDetailUtils.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,78 +24,41 @@ public class ProvinceCityDetailUtils {
 		List<String> zoneList = loadDataFromClassPathResource("utils/zone.txt");
 		List<String> streetList = loadDataFromClassPathResource("utils/street.txt");
 		List<String> stayList = loadDataFromClassPathResource("utils/stayV5.txt");
-//		for (int j = 0; j < cityList.size(); j ++ ) {
-//			String[] cityStr = cityList.get(j).split("\t");
-//			City city = new City();
-//			city.setCityCode(cityStr[0]);
-//			city.setCityName(cityStr[1]);
-//			List<Zone> subZoneList = new ArrayList<>();
-//			for (int k = 0; k < zoneList.size(); k ++ ) {
-//				String[] zoneStr = zoneList.get(k).split("\t");
-//				if (cityStr[0].substring(0, 4).equalsIgnoreCase(zoneStr[0].substring(0, 4))) {
-//					Zone zone = new Zone();
-//					zone.setZoneCode(zoneStr[0]);
-//					zone.setZoneName(zoneStr[1]);
-//					List<Street> subStreetList = new ArrayList<>();
-//					for (int m = 0; m < streetList.size(); m ++ ) {
-//						String[] streetStr = streetList.get(m).split("\t");
-//						if (zoneStr[0].substring(0, 6).equalsIgnoreCase(streetStr[0].substring(0, 6))) {
-//							Street street = new Street();
-//							street.setStreetCode(streetStr[0]);
-//							street.setStreetName(streetStr[1]);
-//							List<Stay> resStayList = new ArrayList<>();
-//							for (int n = 0; n < stayList.size(); n ++ ) {
-//								String[] stayStr = stayList.get(n).split("\t");
-//								if (streetStr[0].equalsIgnoreCase(stayStr[0])) {
-//									List<String> subStayList = new ArrayList<>();
-//									for (int s = 1; s < stayStr.length; s ++ ) {
-//										subStayList.add(stayStr[s]);
-//									}
-//									Stay stay = new Stay();
-//									stay.setStayCode(stayStr[0]);
-//									stay.setStayDetail(subStayList);
-//									resStayList.add(stay);
-//									break;
-//								}
-//							}
-//							street.setStay(resStayList);
-//							subStreetList.add(street);
-//							zone.setStreet(subStreetList);
-//							subZoneList.add(zone);
-//							city.setZone(subZoneList);
-//                            System.out.println(city.toString());
-//						}
-//					}
-//				}
-//			}
-//		}
+	
 		
 		List<Zone> subZoneList = zoneStreetStayInfo(zoneList, streetList, stayList);
-//		for (int i = 0; i <provinceList.size(); i ++ ) {
-//			Province province = new Province();
-//			province.setProvinceCode(provinceList.get(i).)
-//		}
-		for (int j = 0; j < cityList.size(); j ++ ) {
-			City city = new City();
-			city.setCityCode(cityList.get(j).split("\t")[0]);
-			city.setCityName(cityList.get(j).split("\t")[1]);
-			
-			
-			for(int k = 0; k < subZoneList.size(); k ++ ) {
-				if (cityList.get(j).split("\t")[0].substring(0, 4)
-						    .equalsIgnoreCase(subZoneList.get(k).getZoneCode().substring(0, 4))) {
-					subZoneList.add(subZoneList.get(k));
-					city.setZone(subZoneList);
+		List<String> provinces = new ArrayList<>();
+		List<String> cities = new ArrayList<>();
+		for (int i = 0; i < provinceList.size(); i ++ ) {
+			Province province = new Province();
+			province.setProvinceCode(provinceList.get(i).split("\t")[0]);
+			province.setProvinceName(provinceList.get(i).split("\t")[1]);
+			List<City> subCityList = new ArrayList<>();
+			for (int j = 0; j < cityList.size(); j ++ ) {
+				
+				if (provinceList.get(i).split("\t")[0].equalsIgnoreCase(cityList.get(j).split("\t")[0].substring(0, 2))) {
+					City city = new City();
+					city.setCityCode(cityList.get(j).split("\t")[0]);
+					city.setCityName(cityList.get(j).split("\t")[1]);
+					
+					List<Zone> subTreeZoneList = new ArrayList<>();
+					for(int k = 0; k < subZoneList.size(); k ++ ) {
+						if (cityList.get(j).split("\t")[0].substring(0, 4)
+								    .equalsIgnoreCase(subZoneList.get(k).getZoneCode().substring(0, 4))) {
+							subTreeZoneList.add(subZoneList.get(k));
+							city.setZone(subTreeZoneList);
+						}
+					}
+					subCityList.add(city);
+					cities.add(new Gson().toJson(city, City.class));
 				}
 			}
-			
-			System.out.println(new Gson().toJson(city, City.class));
+			province.setCity(subCityList);
+			provinces.add(new Gson().toJson(province, Province.class));
 		}
 		
-		
-		for(Zone z : subZoneList) {
-			System.out.println(new Gson().toJson(z, Zone.class));
-		}
+		IOUtils.writeLines(provinces, null, new FileOutputStream(new File("/home/h/provinces.txt")));
+		IOUtils.writeLines(cities, null, new FileOutputStream("/home/h/cities.txt"));
 	}
 	
 	public static List<Zone> zoneStreetStayInfo(List<String> zoneList, List<String> streetList, List<String> stayList) {

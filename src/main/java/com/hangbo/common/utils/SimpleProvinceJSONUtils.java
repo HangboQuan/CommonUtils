f7a3923866e12c2aed0a8458a2c8f4a9e93b443a
quanhangbo <2409711401@qq.com>
2022-09-12 19:29:21 +0800

    add province-city-zone 3 tree

--- src/main/java/com/hangbo/common/utils/SimpleProvinceJSONUtils.java
+++ src/main/java/com/hangbo/common/utils/SimpleProvinceJSONUtils.java
@@ -25,44 +25,8 @@ public class SimpleProvinceJSONUtils {
 		List<String> streetList = loadDataFromClassPathResource("utils/street.txt");
 		List<String> stayList = loadDataFromClassPathResource("utils/stay.txt");
 		provinceInfo(provinceList, cityList, zoneList, streetList, stayList);
+		zoneInfo(provinceList, cityList, zoneList);
 	}
-	
-//	public static List<Province.City.Zone> zoneStreetStayInfo(List<String> zoneList, List<String> streetList, List<String> stayList) {
-//		List<Province.City.Zone> zones = new ArrayList<>();
-//		for(int i = 0; i < zoneList.size(); i ++ ) {
-//			String[] zoneStr = zoneList.get(i).split("\t");
-//			Province.City.Zone zone = new Province.City.Zone();
-//			zone.setZone(zoneStr[1]);
-//			List<Province.City.Zone.Street> streets = new ArrayList<>();
-//			for(int j = 0; j < streetList.size(); j ++ ) {
-//				String[] streetStr = streetList.get(j).split("\t");
-//				if(zoneStr[0].substring(0, 6).equals(streetStr[0].substring(0, 6))) {
-//					Province.City.Zone.Street street = new Province.City.Zone.Street();
-//					street.setStreet(streetStr[1]);
-//					List<Province.City.Zone.Street.Stay> stays = new ArrayList<>();
-//					for(int k = 0; k < stayList.size(); k ++ ) {
-//						String[] subStayList = stayList.get(k).split("\t");
-//						Province.City.Zone.Street.Stay stay = new Province.City.Zone.Street.Stay();
-//						if(subStayList[0].equals(streetStr[0])) {
-//							List<String> subList = new ArrayList<>();
-//							for(int m = 1; m < subStayList.length; m ++ ) {
-//								subList.add(subStayList[m]);
-//							}
-//							stay.setStay(subList);
-//							stays.add(stay);
-//							break;
-//						}
-//					}
-//					street.setStays(stays);
-//					streets.add(street);
-//				}
-//			}
-//			zone.setStreets(streets);
-//			System.out.println(new Gson().toJson(zone, Province.City.Zone.class));
-//			zones.add(zone);
-//		}
-//		return zones;
-//	}
 	
 	public static void provinceInfo(List<String> provinceList, List<String> cityList, List<String> zoneList,
 	                                List<String> streetList, List<String> stayList){
@@ -132,6 +96,45 @@ public class SimpleProvinceJSONUtils {
 		}
 	}
 	
+	public static void zoneInfo(List<String> provinceList, List<String> cityList, List<String> zoneList) {
+		List<String> provinces = new ArrayList<>();
+		List<String> cityJSON = new ArrayList<>();
+		for(int x = 0; x < provinceList.size(); x ++ ) {
+			String[] provinceStr = provinceList.get(x).split("\t");
+			Province2 province2 = new Province2();
+			province2.setProvince(provinceStr[1]);
+			List<Province2.City> cities = new ArrayList<>();
+			for(int y = 0; y < cityList.size(); y ++ ) {
+				String[] cityStr = cityList.get(y).split("\t");
+				if(provinceStr[0].substring(0, 2).equals(cityStr[0].substring(0, 2))) {
+					Province2.City city = new Province2.City();
+					city.setCity(cityStr[1]);
+					Province2.City.Zone zone = new Province2.City.Zone();
+					List<Province2.City.Zone> zones = new ArrayList<>();
+					List<String> subZones = new ArrayList<>();
+					for(int i = 0; i < zoneList.size(); i ++ ) {
+						String[] zoneStr = zoneList.get(i).split("\t");
+						if(cityStr[0].substring(0, 4).equals(zoneStr[0].substring(0, 4))){
+							subZones.add(zoneStr[1]);
+						}
+					}
+					zone.setZone(subZones);
+					zones.add(zone);
+					city.setZones(zones);
+					cityJSON.add(new Gson().toJson(city, Province2.City.class));
+					cities.add(city);
+				}
+			}
+			province2.setCities(cities);
+			System.out.println(new Gson().toJson(province2, Province2.class));
+			provinces.add(new Gson().toJson(province2, Province2.class));
+		}
+		try{
+			IOUtils.writeLines(provinces, null, new FileOutputStream(new File("/home/h/provinces_cities_zones.txt")));
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
 	public static List<String> loadDataFromClassPathResource(String path) {
@@ -144,9 +147,11 @@ public class SimpleProvinceJSONUtils {
 		}
 		return null;
 	}
-	
-	
-	
+
+
+	/**
+	 * 保留5级关系 省-市-区-街道-居委会
+	 */
 	@Data
 	@Accessors
 	public static class Province {
@@ -179,6 +184,31 @@ public class SimpleProvinceJSONUtils {
 						List<String> stay;
 					}
 				}
+			}
+		}
+	}
+
+
+	/**
+	 * 保留3级关系 省-市-区
+	 */
+	@Data
+	@Accessors
+	public static class Province2 {
+		
+		String province;
+		List<City> cities;
+		
+		@Data
+		@Accessors
+		static class City{
+			String city;
+			List<Province2.City.Zone> zones;
+			
+			@Data
+			@Accessors
+			static class Zone {
+				List<String> zone;
 			}
 		}
 	}
